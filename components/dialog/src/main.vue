dc7ab3dae7c960c96b018db4ef49151190bc422e
kattzhang <1193055507@qq.com>
2020-06-10 20:01:44 +0800

    init dialog

---
+++ components/dialog/src/main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sky-dialog__wrapper">
+    <div class="sky-dialog">
+      <div class="sky-dialog__header">
+        <span class="sky-dialog__title">{{ title }}</span>
+      </div>
+      <div class="sky-dialog__body" >
+        <slot></slot>
+      </div>
+      <div class="sky-dialog__footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkyDialog',
+
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="sass">
+  .sky-dialog__wrapper {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+  }
+</style>
